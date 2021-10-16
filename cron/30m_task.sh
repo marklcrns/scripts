3be67887ef03115736cbbab1d22dc5fb10ff1a81
Mark Lucernas <marklcrns@gmail.com>
2021-10-16 07:29:39 -0700

    refactor(cron) disable task sync

--- cron/30m_task.sh
+++ cron/30m_task.sh
@@ -8,10 +8,9 @@
 PATH=$HOME/.local/bin:/usr/local/bin:/usr/bin:$PATH
 export PATH
 
-LOCK="/tmp/task_sync.lock"
-
-if command -v task && command -v trellowarrior && test ! -e $LOCK; then
-  touch $LOCK
-  trellowarrior sync && task sync
-  rm $LOCK
-fi
+# LOCK="/tmp/task_sync.lock"
+# if command -v task && command -v trellowarrior && test ! -e $LOCK; then
+#   touch $LOCK
+#   trellowarrior sync && task sync
+#   rm $LOCK
+# fi
